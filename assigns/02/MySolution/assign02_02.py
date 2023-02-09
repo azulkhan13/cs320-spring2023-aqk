fcb4c8c2f9365527a93d4339cca115228957040f
Abdul Khan <aqk@bu.edu>
2023-02-09 01:10:48 -0500

    worked on second file

--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -8,5 +8,43 @@ print("[import ./../assign02.py] is done!")
 #
 # Please implement (10 points)
 # mylist_quicksort (see list_quicksort in assign02.sml)
-#
+def qpart(xs,p0):
+    
+    if mylist_nilq(xs):
+        
+        return (mylist_nil(),mylist_nil())
+    
+    else:
+        x1=xs.get_cons1()
+        
+        (ys,zs)= qpart(xs.get_cons2(), p0)
+        
+        if x1 <= p0:
+            
+            return(mylist_cons(x1,ys),zs)
+        
+        else:
+            
+            return (ys,mylist_cons(x1,zs))
+
+def mylist_quicksort(xs):
+    
+    if mylist_nilq(xs):
+        
+        return mylist_nil()
+    
+    else:
+        
+        x1 = xs.get_cons1()
+        
+        (ys,zs) = qpart(xs.get_cons2(),x1)
+        
+        ys=mylist_quicksort(ys)
+        
+        zs=mylist_quicksort(zs)
+        
+        return mylist_append(ys, mylist_cons(x1,zs))
+        
+        
+        
 ####################################################
