53250c7369c9d9ccd08c9994f194e210ec0715c3
Abdul Khan <aqk@bu.edu>
2023-03-24 18:44:00 -0400

    adding support for 2

--- assigns/05/MySolution/Python/assign05_02.py
+++ assigns/05/MySolution/Python/assign05_02.py
@@ -17,6 +17,37 @@ def word_neighbors(word):
     Your implementation should be combinator-based very
     much like the posted solution.
     """
-    raise NotImplementedError
+    def subs(word, i):
+        
+        return word[i]
+
+    def lenss(word):
+        
+        return string_foldleft(word,0,lambda r, x: r + 1)
+
+    def imps(wlist):
+        
+        return foreach_to_foldleft(fnlist_foreach)(wlist, "", lambda r, x0: r + x0)
+
+    def asst(length, fopr):
+        
+        return fnlist_reverse\
+            (int1_foldleft(length,fnlist_nil(), lambda r, x1: fnlist_cons(fopr(x1),r)))
+
+    def string_tabulate(length,fopr):
+        
+        return imps(asst(length,fopr))
+                
+    def    AB():
+        
+        return "abcdefghijklmnopqrstuvwxyz"
+
+    wlen = lenss(word)
+
+    return \
+        fnlist_concat(string_imap_fnlist\
+                      (word, lambda i, c: fnlist_concat\
+                       (string_imap_fnlist(AB(), lambda _, c1: fnlist_sing\
+                                           (string_tabulate(wlen, lambda j: subs(word, j) if i != j else c1)) if (c != c1) else fnlist_nil()))))
 #
 ####################################################
